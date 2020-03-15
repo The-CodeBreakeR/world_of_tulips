6ec1a11d652a40c048ce6aea853cf22d2f694a63
ginetrix <voskobojnikov.artemij@gmail.com>
2020-03-15 13:50:25 -0700

    clean-up unused constructor

--- src/LoggedInHome.js
+++ src/LoggedInHome.js
@@ -5,10 +5,6 @@ import AppFooter from './AppFooter'
 
 class LoggedInHome extends Component {
 
-	constructor(props){
-		super(props);
-	}
-
     render() {
         return <div className='app'>
             <AppHeader {...this.props}/>
