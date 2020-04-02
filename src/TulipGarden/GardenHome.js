f0b07d04d55ec3da7663659a040152fce2407fab
ginetrix <voskobojnikov.artemij@gmail.com>
2020-04-02 16:59:30 -0700

    updated gardenhome

--- src/TulipGarden/GardenHome.js
+++ src/TulipGarden/GardenHome.js
@@ -10,10 +10,13 @@ class GardenHome extends Component {
 
 	constructor(props){
 		super(props);
-    this.loadTulips();
+    this.getAllTulips();
     this.getBulbNumber();
+  }
+
+  componentDidMount(){
     this.digBulb = this.digBulb.bind(this);
-    this.getTulip = this.getTulip.bind(this);
+    this.getTulip = this.getTulip.bind(this);  
   }
 
   // Initialize the respective elements, state to indicate loading
@@ -21,44 +24,39 @@ class GardenHome extends Component {
 
   async getBulbNumber(){
     this.setState ({loading: true})
-    var num = await this.props.worldOfTulips.methods.getUnderGroundBulbNum().call().then(this.setState ({loading: false}));
-    this.setState({num : num});
-  }
-
-  // Load the tulips for the account that is currenlty logged in
-  async loadTulips(){
-    // We load the tulips with IDs 0 to 9, as a placeholder, later we need a dedicated function
-    for (var i = 1; i < 10; i++){
-      const tulip = await this.getTulip(i);
-      if (tulip) {
-        this.setState({
-          tulips:[...this.state.tulips, tulip]
-        });
-      }
-    }
-    this.setState ({loading: false})
+    var num = await this.props.worldOfTulips.methods.getUnderGroundBulbNum().call();
+    this.setState({
+      num : num,
+      loading: false
+    });
   }
 
   async getAllTulips(){
     this.setState({loading: true});
-    const gasAmount = await this.props.worldOfTulips.methods.getAllOwnedTulipIDs(this.props.userAccount).estimateGas({from: this.props.userAccount});
-    var ownedTulips = await this.props.worldOfTulips.methods.getAllOwnedTulipIDs(this.props.userAccount).send({from: this.props.userAccount, gas: gasAmount}).then(this.setState ({loading: false}));
-    this.setState({
-      tulips: ownedTulips
-    });
+    var ownedTulipIDs = await this.props.worldOfTulips.methods.getAllOwnedTulipIDs(this.props.userAccount).call({from: this.props.userAccount});
+    for (var i = 0; i < ownedTulipIDs.length ;i++) {
+        const tulip = await this.getTulip(ownedTulipIDs[i]);
+        console.log(tulip);
+        this.setState({
+          tulips:[...this.state.tulips, tulip]
+        });
+      }
+    this.setState({ loading: false });
   } 
 
   async digBulb(){
     this.setState ({loading: true});
     const gasAmount = await this.props.worldOfTulips.methods.digToFindBulb().estimateGas({from: this.props.userAccount});
-    var bulbId = await this.props.worldOfTulips.methods.digToFindBulb().send({from: this.props.userAccount, gas: gasAmount}).then(this.setState ({loading: false}));
+    var bulbId = await this.props.worldOfTulips.methods.digToFindBulb().send({from: this.props.userAccount, gas: gasAmount});
+    this.setState({ loading: false });
     this.getBulbNumber();
     this.getTulip(bulbId.events.Generation0BulbFound.returnValues.bulbID);
   }
 
   async getTulip(id){
     this.setState ({loading: true});
-    var tulip = await this.props.worldOfTulips.methods.getTulip(id).call().then(this.setState ({loading: false}));
+    var tulip = await this.props.worldOfTulips.methods.getTulip(id).call();
+    this.setState({ loading: false });
     return tulip;
   }
 
